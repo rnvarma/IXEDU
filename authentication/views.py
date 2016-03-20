2a76d9e816290ce93ab19f1edeb009272396f7d7
rnvarma <rohan@prompthire.com>
2016-03-20 13:21:00 -0400

    weoo

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.base import View
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'home.html')
