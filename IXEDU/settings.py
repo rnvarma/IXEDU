6394c8b260eaf2cf2b1bbd20d7719d386b9b4687
rnvarma <rohan@prompthire.com>
2016-03-20 13:05:00 -0400

    woo

--- IXEDU/settings.py
+++ IXEDU/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(AUTH_DIR, 'static'),
     os.path.join(FORUM_DIR, 'static'),
     os.path.join(SE_DIR, 'static'),
