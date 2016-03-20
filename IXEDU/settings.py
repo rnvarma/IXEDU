0cfe2fb2cae008a40bb77be58eccdb364919bcae
rnvarma <rohan@prompthire.com>
2016-03-20 12:57:34 -0400

    added whitenoise static file serving

--- IXEDU/settings.py
+++ IXEDU/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(AUTH_DIR, 'static'),
